73f824ef8e6178a06f8b8f085137e4b916df3ba7
IlkhamRakhimov <amirilkhom@gmail.com>
2024-09-23 11:11:29 +0500

    Version 2

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body bgcolor="#225588">
-    V1
+<body bgcolor="#778899">
+    V2
 </body>
 </html>
